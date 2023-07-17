3fd6ab6ed68601e953093e6881de3cca1bd176ce
Caleb Bryant <caleb.bryant@cyderes.com>
2023-07-17 14:56:23 -0500

    Update test_parsers.py

--- src/test_parsers.py
+++ src/test_parsers.py
@@ -1,5 +1,6 @@
-import os
+import os, re
 from Grammar import Grammar
+from pyparsing import exceptions
 
 standard_parsers_dir = "/Users/caleb.bryant/Library/CloudStorage/OneDrive-Cyderes/TelEng/github/cyderes-parsers/standard"
 override_parsers_dir = "/Users/caleb.bryant/Library/CloudStorage/OneDrive-Cyderes/TelEng/github/cyderes-parsers/overrides"
@@ -13,38 +14,45 @@ results = open('results.txt', 'w')
 results.write("Parsing results\n")
 
 language_grammar = Grammar()
-
-for path, folders, filenames in standard_parsers_folders:
-    if path[-5:] == '/conf':
-        for name in filenames:
-            print(f"{path}/{name}")
-            results.write(f"{path}/{name}\n")
-            parse_attempt = language_grammar.parse_file(f"{path}/{name}")
-            if not parse_attempt:
-                exit(0)
-
-for path, folders, filenames in override_parsers_folders:
-    if path[-5:] == '/conf':
-        for name in filenames:
-            print(f"{path}/{name}")
-            results.write(f"{path}/{name}\n")
-            parse_attempt = language_grammar.parse_file(f"{path}/{name}")
-            if not parse_attempt:
-                exit(0)
-
-for path, folders, filenames in default_parsers_folders:
-    if len(folders) == 0:
-        for name in filenames:
-            print(f"{path}/{name}")
-            results.write(f"{path}/{name}\n")
-            parse_attempt = language_grammar.parse_file(f"{path}/{name}")
-            if not parse_attempt:
-                exit(0)
-    else:
-        for folder in folders:
+try:
+    for path, folders, filenames in standard_parsers_folders:
+        if path[-5:] == '/conf':
             for name in filenames:
                 print(f"{path}/{name}")
                 results.write(f"{path}/{name}\n")
-                parse_attempt = language_grammar.parse_file(f"{path}/{folder}/{name}")
-                if not parse_attempt:
-                    exit(0)
+                language_grammar.parse_file(f"{path}/{name}")
+
+    for path, folders, filenames in override_parsers_folders:
+        if path[-5:] == '/conf':
+            for name in filenames:
+                print(f"{path}/{name}")
+                results.write(f"{path}/{name}\n")
+                language_grammar.parse_file(f"{path}/{name}")
+
+    for path, folders, filenames in default_parsers_folders:
+        if len(folders) == 0:
+            for name in filenames:
+                print(f"{path}/{name}")
+                results.write(f"{path}/{name}\n")
+                language_grammar.parse_file(f"{path}/{name}")
+        else:
+            for folder in folders:
+                for name in filenames:
+                    print(f"{path}/{name}")
+                    results.write(f"{path}/{name}\n")
+                    language_grammar.parse_file(f"{path}/{folder}/{name}")
+
+except exceptions.ParseSyntaxException as oopsie:
+    match = re.search("found '(.*)'", str(oopsie))
+    if match:
+        found = match.group(1)
+    else:
+        found = ''
+    char_num = oopsie.loc
+    line_num = oopsie.lineno
+    col_num = oopsie.col
+    print(oopsie.explain())
+    print(f"ParseSyntaxException: Found unexpected token '{found}' at (line:{line_num}, col:{col_num}), char: {char_num}")
+    print(oopsie.line)
+    print(" " * (oopsie.column - 1) + "^")
+    exit(1)
