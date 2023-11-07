b0ade987d630f7d636e17b0e6dfda88daa36ca9d
slijboom-cyderes <tyler.slijboom@cyderes.com>
2023-11-07 08:39:44 -0500

    commented out unneeded line

--- src/lint_parser.py
+++ src/lint_parser.py
@@ -30,7 +30,7 @@ if config_file:
         #ast2 = AST(None)
         the_state = ast.get_state()
         print(f" state: {the_state}")
-        state = State(ast)
+        #state = State(ast)
 
     except exceptions.ParseSyntaxException as oopsie:
         match = re.search("found '(.*)'", str(oopsie))
